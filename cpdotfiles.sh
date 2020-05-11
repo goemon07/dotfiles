62118724976c749e3a197f1b8a6ca5331d8e0e62
Gabriele Orazi <gabriorazi07@gmail.com>
2020-05-12 01:18:17 +0300

    fix incomplete path for file path

--- cpdotfiles.sh
+++ cpdotfiles.sh
@@ -6,10 +6,9 @@ DIR="$HOME/dotfiles"
 input="$DIR/paths"
 while read line; do
     if [[ ! -z "${line// }" ]]; then
-        echo $line
         if [[ -f "$HOME$line" ]]; then
             if [[ ! -d "$DIR${line%/*}" ]]; then mkdir $DIR${line%/*}; fi
-            cp $HOME$line $DIR
+            cp $HOME$line $DIR${line%/*}
             echo "OK: $line file copied."
         elif [[ -d "$HOME$line" ]]; then
             if [[ ! -d "$DIR$line" ]]; then mkdir $DIR$line; fi
